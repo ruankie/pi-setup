9a86cabf122ba9ad60bc67267d175aa6649ebd43
Ruan Pretorius <ruan@Ruans-MacBook-Pro.local>
2022-05-31 11:43:52 +0200

    updated docker to add non-root user to docker group

--- 3.0-docker.sh
+++ 3.0-docker.sh
@@ -4,3 +4,14 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 # Install Docker
 curl -sSL https://get.docker.com | sh
+
+# Add a pi (non-root) User to the Docker Group
+sudo usermod -aG docker ${USER}
+
+# Enable the Docker system service to start your containers on boot
+sudo systemctl enable docker
+
+# prompt for testing
+echo "successfully installed docker!"
+echo "run the following command to test it:"
+echo "docker run hello-world"
---
+++ 4.0-docker-compose.sh
@@ -0,0 +1,8 @@
+## from https://dev.to/elalemanyo/how-to-install-docker-and-docker-compose-on-raspberry-pi-1mo
+# install python 3 and pip if you haven't yet
+# sudo apt-get install libffi-dev libssl-dev
+# sudo apt install python3-dev
+# sudo apt-get install -y python3 python3-pip
+
+# Install Docker-Compose
+sudo pip3 install docker-compose
