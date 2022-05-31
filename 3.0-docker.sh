c67717d78221c7104dfd934fd72167711bc02bee
Ruan Pretorius <ruan@Ruans-MacBook-Pro.local>
2022-05-31 11:00:59 +0200

    added docker setup

---
+++ 3.0-docker.sh
@@ -0,0 +1,6 @@
+## from https://dev.to/elalemanyo/how-to-install-docker-and-docker-compose-on-raspberry-pi-1mo
+# Update and Upgrade
+sudo apt-get update && sudo apt-get upgrade
+
+# Install Docker
+curl -sSL https://get.docker.com | sh
