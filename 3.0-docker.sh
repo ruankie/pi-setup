4d8778f100c4fc2418783ab7900724bf5745e3fc
Ruan Pretorius <ruan@Ruans-MacBook-Pro.local>
2022-05-31 11:28:52 +0200

    added automatic accept

--- 3.0-docker.sh
+++ 3.0-docker.sh
@@ -1,6 +1,6 @@
 ## from https://dev.to/elalemanyo/how-to-install-docker-and-docker-compose-on-raspberry-pi-1mo
 # Update and Upgrade
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
 
 # Install Docker
 curl -sSL https://get.docker.com | sh
